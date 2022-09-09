b80cf2810090a6b3a459aff63ac24dfd7c805e0c
ErJump <fedelijunior@gmail.com>
2022-09-09 14:15:41 +0200

    feat: :sparkles: Adds TrainsController + Sets route + Adds trains/index.blade.php

---
+++ resources/views/trains/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Trains</title>
+</head>
+<body>
+    <main>
+        <h1>Trains:</h1>
+        <ul>
+            @foreach ($trains as $train)
+                <li>
+                    {{ $train->company }}
+                </li>
+            @endforeach
+        </ul>
+    </main>
+</body>
+</html>
